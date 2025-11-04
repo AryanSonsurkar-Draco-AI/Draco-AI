8e3434826a159c60a7d181139ce3f574957adfcb
aryansonsurkar <boyalone28405@gmail.com>
2025-11-04 21:07:36 +0530

    Added Procfile and Render setup

--- main.py
+++ main.py
@@ -671,4 +671,5 @@ if __name__ == "__main__":
 
     # start Flask SocketIO server
     print("Starting Draco server on http://127.0.0.1:5000")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
